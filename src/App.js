d0d4f73dd06106a011cb6b1ac3e81927ff8873f3
IfzaRasool <ifzarasoolarain@gmail.com>
2022-04-19 22:40:16 +0300

    Add calculation class - Add style

---
+++ src/App.js
@@ -0,0 +1,10 @@
+/* eslint-disable react/prefer-stateless-function */
+import React from 'react';
+import Calculator from './components/calculator';
+
+class App extends React.Component {
+  render() {
+    return (<Calculator />);
+  }
+}
+export default App;

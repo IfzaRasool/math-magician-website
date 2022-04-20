42091589fadc24d14621e5420c298eeef1b32991
IfzaRasool <ifzarasoolarain@gmail.com>
2022-04-20 22:13:31 +0300

    Add event and logic

--- src/components/calculator.js
+++ src/components/calculator.js
@@ -1,36 +1,56 @@
 /* eslint-disable react/prefer-stateless-function */
 import React from 'react';
 import calculate from '../logic/calculate';
-import operate from '../logic/operate';
 
 class Calculator extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      total: 0,
+      next: null,
+      operation: null,
+    };
+    this.handleClick = this.handleClick.bind(this);
+  }
+
+  handleClick(btn) {
+    const btnData = btn.target.textContent;
+    this.setState((event) => calculate(event, btnData));
+  }
+
   render() {
+    const { total, next, operation } = this.state;
+
     return (
       <div className="calculator">
-        <input type="text" className="cal-screen" value="0" disabled />
+        <div className="cal-screen">
+          { total}
+          { operation}
+          { next }
+        </div>
         <div className="cal-buttons">
-          <button type="button" className="btn">AC</button>
-          <button type="button" className="btn">+/-</button>
-          <button type="button" className="btn">%</button>
-          <button type="button" className="btn primary">/</button>
-          <button type="button" className="btn">7</button>
-          <button type="button" className="btn">8</button>
-          <button type="button" className="btn">9</button>
-          <button type="button" className="btn primary">*</button>
-          <button type="button" className="btn">4</button>
-          <button type="button" className="btn">5</button>
-          <button type="button" className="btn">6</button>
-          <button type="button" className="btn primary">-</button>
-          <button type="button" className="btn">1</button>
-          <button type="button" className="btn">2</button>
-          <button type="button" className="btn">3</button>
-          <button type="button" className="btn primary">+</button>
-          <button type="button" className="btn zero">0</button>
-          <button type="button" className="btn">.</button>
-          <button type="button" className="btn primar">=</button>
+          <button type="button" className="btn" onClick={this.handleClick}>AC</button>
+          <button type="button" className="btn" onClick={this.handleClick}>+/-</button>
+          <button type="button" className="btn" onClick={this.handleClick}>%</button>
+          <button type="button" className="btn primary" onClick={this.handleClick}>÷</button>
+          <button type="button" className="btn" onClick={this.handleClick}>7</button>
+          <button type="button" className="btn" onClick={this.handleClick}>8</button>
+          <button type="button" className="btn" onClick={this.handleClick}>9</button>
+          <button type="button" className="btn primary" onClick={this.handleClick}>x</button>
+          <button type="button" className="btn" onClick={this.handleClick}>4</button>
+          <button type="button" className="btn" onClick={this.handleClick}>5</button>
+          <button type="button" className="btn" onClick={this.handleClick}>6</button>
+          <button type="button" className="btn primary" onClick={this.handleClick}>-</button>
+          <button type="button" className="btn" onClick={this.handleClick}>1</button>
+          <button type="button" className="btn" onClick={this.handleClick}>2</button>
+          <button type="button" className="btn" onClick={this.handleClick}>3</button>
+          <button type="button" className="btn primary" onClick={this.handleClick}>+</button>
+          <button type="button" className="btn zero" onClick={this.handleClick}>0</button>
+          <button type="button" className="btn" onClick={this.handleClick}>.</button>
+          <button type="button" className="btn primary" onClick={this.handleClick}>=</button>
         </div>
       </div>
     );
   }
 }
-export default { Calculator, calculate, operate };
+export default Calculator;

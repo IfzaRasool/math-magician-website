1183f02ca04408eabe9c531064c432ee7d15aaa8
IfzaRasool <ifzarasoolarain@gmail.com>
2022-04-21 14:23:07 +0300

    remove commented code

--- src/components/calculator.js
+++ src/components/calculator.js
@@ -54,28 +54,6 @@ class Calculator extends Component {
             <CreateButton key={btn} value={btn} handleClick={this.handleClick} />
           ))}
         </div>
-
-        { /* <div className="cal-buttons">
-          <button type="button" className="btn" onClick={this.handleClick}>AC</button>
-          <button type="button" className="btn" onClick={this.handleClick}>+/-</button>
-          <button type="button" className="btn" onClick={this.handleClick}>%</button>
-          <button type="button" className="btn primary" onClick={this.handleClick}>÷</button>
-          <button type="button" className="btn" onClick={this.handleClick}>7</button>
-          <button type="button" className="btn" onClick={this.handleClick}>8</button>
-          <button type="button" className="btn" onClick={this.handleClick}>9</button>
-          <button type="button" className="btn primary" onClick={this.handleClick}>x</button>
-          <button type="button" className="btn" onClick={this.handleClick}>4</button>
-          <button type="button" className="btn" onClick={this.handleClick}>5</button>
-          <button type="button" className="btn" onClick={this.handleClick}>6</button>
-          <button type="button" className="btn primary" onClick={this.handleClick}>-</button>
-          <button type="button" className="btn" onClick={this.handleClick}>1</button>
-          <button type="button" className="btn" onClick={this.handleClick}>2</button>
-          <button type="button" className="btn" onClick={this.handleClick}>3</button>
-          <button type="button" className="btn primary" onClick={this.handleClick}>+</button>
-          <button type="button" className="btn zero" onClick={this.handleClick}>0</button>
-          <button type="button" className="btn" onClick={this.handleClick}>.</button>
-          <button type="button" className="btn primary" onClick={this.handleClick}>=</button>
-</div> */ }
       </div>
     );
   }
